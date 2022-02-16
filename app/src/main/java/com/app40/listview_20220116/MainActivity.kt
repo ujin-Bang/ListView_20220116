e34cbc32d174a5545ace9bf1c3a83deae601614d
ujin_bang <qkddnwls14@gmail.com>
2022-02-16 16:03:41 +0900

    어댑터를 실제로 만들어서 > 리스트뷰와 연결

--- app/src/main/java/com/app40/listview_20220116/MainActivity.kt
+++ app/src/main/java/com/app40/listview_20220116/MainActivity.kt
@@ -2,10 +2,13 @@ package com.app40.listview_20220116
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.app40.listview_20220116.adapters.StudentAdapter
 import com.app40.listview_20220116.datas.StudentData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var mAdapter: StudentAdapter
     val mStudentList = ArrayList<StudentData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,14 +16,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         mStudentList.add( StudentData("조경진",1988,"서울시 동대문구"))
+        mStudentList.add( StudentData("고재천",1988,"서울시 중랑구"))
+        mStudentList.add( StudentData("방우진",1983,"서울시 고양시"))
+        mStudentList.add( StudentData("서정민",1984,"서울시 광명시"))
+        mStudentList.add( StudentData("이승민",1994,"서울시 안양시"))
+        mStudentList.add( StudentData("고재천",1988,"서울시 강서구"))
+        mStudentList.add( StudentData("고재천",1988,"서울시 송파구"))
         mStudentList.add( StudentData("고재천",1988,"서울시 동대문구"))
-        mStudentList.add( StudentData("방우진",1983,"서울시 동대문구"))
-        mStudentList.add( StudentData("서정민",1984,"서울시 동대문구"))
         mStudentList.add( StudentData("고재천",1988,"서울시 동대문구"))
         mStudentList.add( StudentData("고재천",1988,"서울시 동대문구"))
-        mStudentList.add( StudentData("고재천",1988,"서울시 동대문구"))
-        mStudentList.add( StudentData("고재천",1988,"서울시 동대문구"))
-        mStudentList.add( StudentData("고재천",1988,"서울시 동대문구"))
-        mStudentList.add( StudentData("고재천",1988,"서울시 동대문구"))
+
+        mAdapter = StudentAdapter(this,R.layout.student_list_item,mStudentList)
+        studentListView.adapter = mAdapter
     }
 }
