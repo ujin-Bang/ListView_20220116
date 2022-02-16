1769cca49433dcd78ac4ba50371eb606045f5355
ujin_bang <qkddnwls14@gmail.com>
2022-02-16 14:35:39 +0900

    Initial commit

---
+++ app/src/main/java/com/app40/listview_20220116/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.app40.listview_20220116
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
