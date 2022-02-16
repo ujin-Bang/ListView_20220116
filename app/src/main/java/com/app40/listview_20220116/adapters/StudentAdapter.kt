48b77ec5f8e8e1e007db2f3a5fe0018c0443014d
ujin_bang <qkddnwls14@gmail.com>
2022-02-16 15:15:28 +0900

    ArrayAdapter 상속을 받을 때, 필요한 재료들을 자녀클래스가 받아서 부모에게 전달.

--- app/src/main/java/com/app40/listview_20220116/adapters/StudentAdapter.kt
+++ app/src/main/java/com/app40/listview_20220116/adapters/StudentAdapter.kt
@@ -1,4 +1,12 @@
 package com.app40.listview_20220116.adapters
 
-class StudentAdapter {
+import android.content.Context
+import android.widget.ArrayAdapter
+import com.app40.listview_20220116.datas.StudentData
+
+class StudentAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<StudentData>
+): ArrayAdapter<StudentData>( mContext,resId,mList) {
 }
